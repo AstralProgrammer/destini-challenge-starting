c4c822781a41167f2ecce4026766c5462d7794ec
Angel <darkangel0938@gmail.com>
2021-07-09 20:10:41 +0800

    #25-29 done. #21-22 fix

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -47,20 +47,29 @@ class StoryBrain {
     return _storyData[_storyNumber].choice2;
   }
 
-  void nextStory(int choiceNumber){
-    if (_storyNumber > 2) {
+  void nextStory(int choiceNumber) {
+    if (choiceNumber == 1 && _storyNumber == 0) {
+      _storyNumber = 2;
+    } else if (choiceNumber == 2 && _storyNumber == 0) {
+      _storyNumber = 1;
+    } else if (choiceNumber == 1 && _storyNumber == 1) {
+      _storyNumber = 2;
+    } else if (choiceNumber == 2 && _storyNumber == 1) {
+      _storyNumber = 3;
+    } else if (choiceNumber == 1 && _storyNumber == 2) {
+      _storyNumber = 5;
+    } else if (choiceNumber == 2 && _storyNumber == 2) {
+      _storyNumber = 4;
+    } else if (_storyNumber > 2) {
       restart();
     }
-    _storyNumber = _storyNumber + choiceNumber;
   }
 
   void restart() {
     _storyNumber = 0;
   }
+
+  bool buttonShouldBeVisible() {
+    return _storyNumber < 3;
+  }
 }
-
-//TODO: Step 25 - Change the storyNumber property into a private property so that only story_brain.dart has access to it. You can do this by right clicking on the name (storyNumber) and selecting Refactor -> Rename to make the change across all the places where it's used.
-
-
-
-//TODO: Step 27 - Create a method called buttonShouldBeVisible() which checks to see if storyNumber is 0 or 1 or 2 (when both buttons should show choices) and return true if that is the case, else it should return false.
